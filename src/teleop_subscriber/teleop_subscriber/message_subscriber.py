44ce89a0549c22de455fbd12e0c2f7af87f2e3d4
mbrickhouse238 <brickhouse.m2309@gmail.com>
2025-10-16 19:17:37 -0400

    added last year resources

---
+++ src/teleop_subscriber/teleop_subscriber/message_subscriber.py
@@ -0,0 +1,61 @@
+# Import the necessary ROS2 libraries
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+# Define a class for your subscriber node, inheriting from the ROS2 Node class
+class MessageSubscriber(Node):
+
+    def __init__(self):
+        # Call the constructor of the parent Node class and give it a name
+        super().__init__('simple_subscriber')
+
+        # Create a subscriber. This is the core of the node's functionality.
+        # self.create_subscription(msg_type, topic_name, callback_function, qos_profile)
+        self.subscription = self.create_subscription(
+            String,             # The message type (std_msgs.msg.String)
+            'input',          # The topic name to subscribe to
+            self.listener_callback, # The function to call when a message is received
+            10)                 # The Quality of Service profile depth
+        
+        # This is just to prevent a "variable not used" warning
+        self.subscription  
+
+        # Log a message to the console indicating the node has started
+        self.get_logger().info('Subscriber node started. Waiting for messages...')
+
+    def listener_callback(self, msg):
+        """
+        This function is called every time a message is published to the 'input' topic.
+        The message data is passed to this function as the 'msg' argument.
+        """
+        # Log the received message data to the console
+        # The 'data' attribute of the String message holds the actual text
+        data = msg.data
+        self.get_logger().info(f'I heard: "{msg.data}"')
+
+        # Placeholder to test
+        # If data = right, 'send current' to right motor, will implement actual sending once I know how to
+        if(data.lower() == "right"):
+            self.get_logger().info('Sendin current to right motor')
+
+
+def main(args=None):
+    # Initialize the rclpy library
+    rclpy.init(args=args)
+
+    # Create an instance of the MessageSubscriber node
+    message_subscriber = MessageSubscriber()
+
+    # "Spin" the node, which means it will keep running and processing callbacks
+    # until it's manually shut down (e.g., with Ctrl+C).
+    rclpy.spin(message_subscriber)
+
+    # When the node is shut down, destroy it and clean up
+    message_subscriber.destroy_node()
+    rclpy.shutdown()
+
+
+# This is the standard entry point for a Python script
+if __name__ == '__main__':
+    main()
